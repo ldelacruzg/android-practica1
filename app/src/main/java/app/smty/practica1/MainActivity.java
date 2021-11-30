ed3f804a1fce3710a042a1590be9e0e21bd35351
delacruz <ldelacruzg@uteq.edu.ec>
2021-11-30 10:59:25 -0500

    Cración de los componentes

--- app/src/main/java/app/smty/practica1/MainActivity.java
+++ app/src/main/java/app/smty/practica1/MainActivity.java
@@ -3,12 +3,22 @@ package app.smty.practica1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editTextName, editTextDateOfBirth, editTextPhone;
+    RadioGroup radioGroupGender;
+    Button buttonSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initUI() {
+
     }
 }
